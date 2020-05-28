30c28183b9d46d9bfeb905f260c043d6b38ec45f
a5chanda <abhinav.chanda99@gmail.com>
2020-05-28 19:41:17 -0400

    Added heroku server uri

--- app/config/env.js
+++ app/config/env.js
@@ -1,1 +1,2 @@
-export const serverURI="http://353d056a.ngrok.io";
+// export const serverURI="http://353d056a.ngrok.io";
+export const serverURI="https://syncro-sessions.herokuapp.com/" //prod server
